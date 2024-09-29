0631c25caa6dd441ef9456e1700b474af4b9a626
jona-max <yonayemane2@gmail.com>
2024-09-29 14:50:28 +0300

    feat:  products store page
    - listing products page with cards
    - sort and search section
    - sidebar for filter by different options

--- webapp/channels/src/components/channel_layout/channel_controller.tsx
+++ webapp/channels/src/components/channel_layout/channel_controller.tsx
@@ -1,9 +1,9 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import classNames from "classnames";
-import React, { lazy, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import classNames from 'classnames';
+import React, {lazy, useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 
 import { cleanUpStatusAndProfileFetchingPoll } from "mattermost-redux/actions/status_profile_polling";
 import { getIsUserStatusesConfigEnabled } from "mattermost-redux/selectors/entities/common";
@@ -35,24 +35,33 @@ import Sidebar from "components/sidebar";
 import CRTPostsChannelResetWatcher from "components/threading/channel_threads/posts_channel_reset_watcher";
 import UnreadsStatusHandler from "components/unreads_status_handler";
 
-const ProductNoticesModal = makeAsyncComponent(
-    "ProductNoticesModal",
-    lazy(() => import("components/product_notices_modal"))
-);
-const ResetStatusModal = makeAsyncComponent(
-    "ResetStatusModal",
-    lazy(() => import("components/reset_status_modal"))
-);
+import {makeAsyncComponent} from 'components/async_load';
+import CenterChannel from 'components/channel_layout/center_channel';
+import LoadingScreen from 'components/loading_screen';
+import ProductResults from 'components/shop/product_results';
+import Sidebar from 'components/sidebar';
+import CRTPostsChannelResetWatcher from 'components/threading/channel_threads/posts_channel_reset_watcher';
+import UnreadsStatusHandler from 'components/unreads_status_handler';
 
-const BODY_CLASS_FOR_CHANNEL = ["app__body", "channel-view"];
-import notesIcon from "../../images/notes-icon.png";
-import storeIcon from "../../images/store.png";
+import Pluggable from 'plugins/pluggable';
+import {Constants} from 'utils/constants';
+import {isInternetExplorer, isEdge} from 'utils/user_agent';
+
+import notesIcon from '../../images/notes-icon.png';
+import storeIcon from '../../images/store.png';
+
+const ProductNoticesModal = makeAsyncComponent('ProductNoticesModal', lazy(() => import('components/product_notices_modal')));
+const ResetStatusModal = makeAsyncComponent('ResetStatusModal', lazy(() => import('components/reset_status_modal')));
+
+const BODY_CLASS_FOR_CHANNEL = ['app__body', 'channel-view'];
 
 type Props = {
     shouldRenderCenterChannel: boolean;
 };
 
 export default function ChannelController(props: Props) {
+    const [openShop, setOpenShop] = useState(false);
+
     const enabledUserStatuses = useSelector(getIsUserStatusesConfigEnabled);
     const dispatch = useDispatch();
 
@@ -123,7 +132,6 @@ export default function ChannelController(props: Props) {
                 className="channel-view"
                 data-testid="channel_view"
             >
-<<<<<<< HEAD
                 <UnreadsStatusHandler />
                 <ProductNoticesModal />
                 <div
@@ -154,18 +162,31 @@ export default function ChannelController(props: Props) {
                     <button onClick={() => setOpenShop(!openShop)}>
                         <img src={storeIcon} />
                     </button>
-=======
-                <UnreadsStatusHandler/>
-                <ProductNoticesModal/>
-                <div className={classNames('container-fluid channel-view-inner')}>
-                    {props.shouldRenderCenterChannel ? (
-                        <CenterChannel/>
+                    {openShop ? (
+                        <ProductResults/>
                     ) : (
-                        <LoadingScreen centered={true}/>
+                        <>
+                            {props.shouldRenderCenterChannel ? (
+                                <CenterChannel/>
+                            ) : (
+                                <LoadingScreen centered={true}/>
+                            )}
+                            <Pluggable pluggableName='Root'/>
+                            <ResetStatusModal/>
+                        </>
                     )}
-                    <Pluggable pluggableName='Root'/>
-                    <ResetStatusModal/>
->>>>>>> 56eb6ca92a (fix(ui): resolved ui issues)
+                </div>
+            </div>
+            <div className='home-screen-wrapper__sidebar right'>
+                <div className='top'>
+                    <button>
+                        <img src={notesIcon}/>
+                    </button>
+                </div>
+                <div className='bottom'>
+                    <button onClick={() => setOpenShop(!openShop)}>
+                        <img src={storeIcon}/>
+                    </button>
                 </div>
             </div>
         </>
